f2fa6e66f8c5c05f6ec868177d1c6fdfd7f3ebac
williamlim16 <williaml1601@gmail.com>
2025-08-26 11:02:53 +1000

    feat: adding better-auth

---
+++ src/server/auth.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+
+export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        required: true,
+        defaultValue: "user",
+        input: false,
+      },
+    },
+  },
+});
